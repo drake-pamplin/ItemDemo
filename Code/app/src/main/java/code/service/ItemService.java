f7578abd730efce4ec68f9c1e134ff724945cf86
Drake Pamplin <drake.pamplin@gmail.com>
2023-08-12 16:37:43 -0500

    chore: created item service and added member methods

---
+++ Code/app/src/main/java/code/service/ItemService.java
@@ -0,0 +1,156 @@
+package code.service;
+
+import java.util.List;
+
+import code.VO.Item;
+import code.exception.InvalidArgException;
+import code.repository.ItemRepository;
+import code.utils.Constants;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ItemService {
+    private ItemRepository itemRepository;
+
+    // Add member to key.
+    public void AddMemberToKey(String key, String member) throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+        
+        // Validate key and member.
+        if (!itemRepository.ContainsKey(key)) {
+            String errorMessage = String.format(Constants.exceptionMessageKeyDoesNotExist, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldKey, errorMessage);
+        }
+        if (itemRepository.ContainsMember(key, member)) {
+            String errorMessage = String.format(Constants.exceptionMessageMemberExists, member, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldMember, errorMessage);
+        }
+
+        // Add member to key.
+        itemRepository.AddMemberToKey(key, member);
+    }
+
+    // Clear all members and keys.
+    public void ClearRepository() {
+        itemRepository.Clear();
+    }
+
+    // Check if a key exists.
+    public boolean ContainsKey(String key) throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Return check.
+        return itemRepository.ContainsKey(key);
+    }
+
+    // Check if a member exists.
+    public boolean MemberExists(String key, String member) throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Validate key exists.
+        if (!itemRepository.ContainsKey(key)) {
+            String errorMessage = String.format(Constants.exceptionMessageKeyDoesNotExist, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldKey, errorMessage);
+        }
+
+        // Return check.
+        return itemRepository.ContainsMember(key, member);
+    }
+
+    // Get all items.
+    public List<Item> GetAllItems() throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Return all items.
+        return itemRepository.GetAllItems();
+    }
+
+    // Get all keys.
+    public List<String> GetAllKeys() throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Return list of keys.
+        return itemRepository.GetAllKeys();
+    }
+
+    // Get all members.
+    public List<String> GetAllMembers() throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Return member list.
+        return itemRepository.GetAllMembers();
+    }
+    
+    // Get all members for a key.
+    public List<String> GetAllMembersForKey(String key) throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Validate key exists.
+        if (!itemRepository.ContainsKey(key)) {
+            String errorMessage = String.format(Constants.exceptionMessageKeyDoesNotExist, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldKey, errorMessage);
+        }
+
+        // Return list of members for key.
+        return itemRepository.GetAllMembersForKey(key);
+    }
+
+    // Remove all members from a key.
+    public void RemoveAllMembersForKey(String key) throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+
+        // Validate key exists.
+        if (!itemRepository.ContainsKey(key)) {
+            String errorMessage = String.format(Constants.exceptionMessageKeyDoesNotExist, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldKey, errorMessage);
+        }
+
+        // Remove members for key.
+        itemRepository.RemoveAllMembersForKey(key);
+    }
+    
+    // Remove member from a key.
+    public void RemoveMemberFromKey(String key, String member) throws InvalidArgException {
+        // Validate items exist.
+        if (!itemRepository.ContainsKeys()) {
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldItemList, Constants.exceptionMessageItemsEmpty);
+        }
+        
+        // Validate key and member.
+        if (!itemRepository.ContainsKey(key)) {
+            String errorMessage = String.format(Constants.exceptionMessageKeyDoesNotExist, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldKey, errorMessage);
+        }
+        if (!itemRepository.ContainsMember(key, member)) {
+            String errorMessage = String.format(Constants.exceptionMessageMemberDoesNotExist, member, key);
+            throw new InvalidArgException(Constants.exceptionMessageGeneric, Constants.fieldMember, errorMessage);
+        }
+
+        // Remove member from key.
+        itemRepository.RemoveMemberForKey(key, member);
+    }
+}
